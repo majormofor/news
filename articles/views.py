fc2a09f251295dc823879206c35edccd60114122
majormofor <uzoumej@gmail.com>
2023-09-12 11:49:40 +0100

    permissions and Authorizations

--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,8 @@ from django.views.generic import DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
-# for creating an article
+# for restricting access using mixins
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # All for cancel button - ArticleCancelView
@@ -18,15 +19,15 @@ from .models import Article
 # Create your views here.
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
         "title",
@@ -34,22 +35,36 @@ class ArticleUpdateView(UpdateView):
     )
     template_name = "article_edit.html"
 
-class ArticleDeleteView(DeleteView):
+    # To block another user from editing another authors post
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
-class ArticleCancelView(View):
+     # To block another user from deleting another authors post
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCancelView(LoginRequiredMixin, View):
     def get(self, request, pk):
         article = get_object_or_404(Article, pk=pk)
         # Add your cancel logic here, if needed
         return redirect(reverse("article_detail", kwargs={"pk": pk}))
     
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
     fields = (
         "title",
         "body",
-        "author",
     )
+
+    # function/method to make sure the author is same without the options of other authors in the database
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
